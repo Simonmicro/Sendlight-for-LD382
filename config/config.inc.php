82b3a7d4b69005ff8714eed2d819417d7a8e84e4
Simon beginn <simon@simonmicro.de>
2019-03-24 12:23:50 +0100

    Added some comments for easier use

--- config/config.inc.php
+++ config/config.inc.php
@@ -3,7 +3,7 @@
  * Please don't use any important password! It will still in clear text after running the webinterface...
 */
 $Config = [
-    'authEnabled' => true,
+    'authEnabled' => true, //Some CGI-configurations will fail with HTTP-Auth, so maybe disable it...
     'authUser' => 'USER',
     'authPassword' => 'PASSWORD',
     'reloadAfterCommand' => true,
@@ -11,18 +11,23 @@ $Config = [
 ];
 
 $ConfigDevices = [
-    '0' => [
-        'DeviceName' => 'Desk lamp',
-        'DeviceIP' => '192.168.0.12',
+    '0' => [ //Make sure to increment only by ONE
+        'DeviceName' => 'Desk lamp 1',
+        'DeviceIP' => '192.168.0.42', //Domains or hostnames are accepted too
+        'DevicePort' => 5577, //Default port, will maybe change
+    ],
+    '1' => [
+        'DeviceName' => 'Desk lamp 2',
+        'DeviceIP' => '192.168.0.43',
         'DevicePort' => 5577,
     ],
 ];
 
 $StarredColors = [
-    '0' => [
-        'Nickname' => 'Max light',
-        'ColorRGB' => '#FF7070',
-        'ColorWW' => '255',
+    '0' => [ //Make sure to increment only by ONE
+        'Nickname' => 'Max light', //Try to make it short
+        'ColorRGB' => '#FF7070', //Plain old HTML code... Maybe copy it out of the selector from the interface
+        'ColorWW' => '255', //0...255 For the value of the of the WW-pin - DON'T SET INVALID VALUES!
     ],
     '1' => [
         'Nickname' => 'Good for work',
@@ -35,5 +40,4 @@ $StarredColors = [
         'ColorWW' => '00',
     ],
 ];
-
 ?>
